d628cf92220fd17a7bfab9091325bbb0d4f96951
drcdh <danielrhunter@gmail.com>
2024-10-14 12:46:37 -0400

    GridSpec plots

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ except UserWarning:
     print("See comment about matplotlib. Exiting")
     exit(0)
 
+from matplotlib.gridspec import GridSpec
 import numpy as np
 import time
 
@@ -44,8 +45,6 @@ def main():
     Y = np.array(np.meshgrid(*(D*(range(K),)))).T.reshape(-1, D)
     X = np.arange(K**C)
 
-    Y_EXP = np.expand_dims(Y, axis=1)
-
     start_time = time.time()
     lr = LR
     nnd_f, nnd_g = [], []
@@ -53,7 +52,7 @@ def main():
     try:
         if g is None:
             for i in range(ITERATIONS):
-                nf_Y = best_match_f(f, Y_EXP)
+                nf_Y = best_match_f(f, Y)
                 df = df_X(lr, i, Y, nf_Y, f, X)
                 print("f(x):  {:3d}, {:.3f} : {:.3f} +- ({:.6f} +- {:.6f})".format(i+1, lr, np.mean(np.abs(f)), np.mean(np.abs(df)), np.std(np.abs(df))))
                 f += df
@@ -88,16 +87,6 @@ def main():
         pass
     if (i+1)%PLOT_ITERATIONS != 0:
         summarize(start_time, i, Y, f, g, nnd_f, nnd_g)
-    #print(f)
-
-#def nn_distance_stats(nnd, map_):
-#    diff = np.subtract.outer(map_, map_)
-#    # don't use inter-channel differences
-#    dist = np.sqrt(diff[:, :, 0, :, :, 0]**2 + diff[:, :, 1, :, :, 1]**2)
-#    dist = np.fill_diagonal(dist.reshape(K**D, K**2), np.inf).reshape(K, K, K, K)
-#    min_dist = np.min(dist)
-#    mean, std  = np.mean(dist), np.std(dist)
-#    nnd.append([mean, std])
 
 def f_nn_distance_stats(nnd, f):
     min_d = []
@@ -106,7 +95,8 @@ def f_nn_distance_stats(nnd, f):
         d[i] = np.inf
         min_d.append(np.min(d))
     min_d = np.array(min_d)
-    nnd.append([np.mean(min_d), np.std(min_d)])
+#    nnd.append([np.mean(min_d), np.std(min_d)])
+    nnd.append([np.median(min_d), np.min(min_d), np.max(min_d)])
 
 def g_nn_distance_stats(nnd, g):
     min_d = []
@@ -115,7 +105,8 @@ def g_nn_distance_stats(nnd, g):
         d[i] = np.inf
         min_d.append(np.min(d))
     min_d = np.array(min_d)
-    nnd.append([np.mean(min_d), np.std(min_d)])
+#    nnd.append([np.mean(min_d), np.std(min_d)])
+    nnd.append([np.median(min_d), np.min(min_d), np.max(min_d)])
 
 def summarize(start_time, i, Y, f, g, nnd_f, nnd_g):
     end_time = time.time()
@@ -123,6 +114,7 @@ def summarize(start_time, i, Y, f, g, nnd_f, nnd_g):
     plot(i, Y, f, g, nnd_f, nnd_g)
 
 def best_match_f(f, Y):
+    Y = np.expand_dims(Y, axis=1)
     Y = Y/(K-1)  # map from [[0, K-1], [0, K-1]] to [[0, 1], [0, 1]]
     return np.argmin(np.sum((f-Y)**2, axis=2), axis=1)
 
@@ -181,69 +173,99 @@ def dg_Y(lr, i, X, g, Y, ng_X):
     return lr * np.sum(dg_X_Y, axis=1).reshape(g.shape)
 
 def plot(i, Y, f, g, nnd_f, nnd_g):
-    Y_EXP = np.expand_dims(Y, axis=1)
-    rows = 2 if f is None or g is None else 3
-    cols = 2 if PLOT_3D else 1
-    index = 1
-    fig = plt.figure(figsize=plt.figaspect(.5))
+    PLOT_F = f is not None
+    PLOT_G = g is not None
+    PLOT_BOTH = PLOT_F and PLOT_G
+
+    fig = plt.figure(layout="constrained")
     fig.suptitle(f"{K=} {SEED=} {SIGMA_A=:.3} {SIGMA_B=:.3}\n{ANNEAL_N=} {ANNEAL_D=} {LR=} {LR_DECAY=} iterations={i+1}")
-    if f is not None:
+
+    gs = GridSpec(2, 4 if PLOT_BOTH else 2, figure=fig)
+
+    if PLOT_F:
         if PLOT_3D:
-            ax = fig.add_subplot(rows, cols, index, projection="3d")
-            ax.set_xlabel("$x$")
-            ax.set_ylabel("$y_0$")
-            ax.set_zlabel("$y_1$")
-            for x, fx in enumerate(f):
-                ax.plot([x, x], [0, fx[0]*(K-1)], [0, fx[1]*(K-1)], "b-")
-            index += 1
-        ax = fig.add_subplot(rows, cols, index)
-        #ax.set_aspect("equal")
-        ax.set_xlabel("$y_0$")
-        ax.set_ylabel("$y_1$")
-        ax.grid()
-        path = Y[np.argsort(best_match_f(f, Y_EXP))]
-        path = path[:,0], path[:,1]
-        ax.plot(*path, ".r-")
-        ax.scatter(f[:,0]*(K-1), f[:,1]*(K-1), c="b", marker="o")
-        index += 1
-    if g is not None:
-        X = np.arange(K**2)
-        if PLOT_3D:
-            ax = fig.add_subplot(rows, cols, index, projection="3d")
-            ax.set_xlabel("$y_0$")
-            ax.set_ylabel("$y_1$")
-            ax.set_zlabel("$x$")
-            X_, Y_ = np.arange(K), np.arange(K)
-            X_, Y_ = np.meshgrid(X_, Y_)
-            ax.plot_surface(X_, Y_, g, cmap=cm.coolwarm)
-            index += 1
-        ax = fig.add_subplot(rows, cols, index)
-        ax.set_xlabel("$y_0$")
-        ax.set_ylabel("$y_1$")
-        ax.grid()
-        path = best_match_g(g, X)
-        path = path[:,1], path[:,0]
-        ax.plot(*path, ".g-")
-        index += 1
-    ax = fig.add_subplot(rows, cols, index)
-    ax.set_xlabel("Iteration")
-    ax.set_ylabel("Distance")
-    ax.grid()
-    if nnd_f or nnd_g:
-        iterations = np.arange(len(nnd_f or nnd_g))
+            ax = fig.add_subplot(gs[0, 0], projection="3d")
+            plot_f_3d(ax, f)
+        ax = fig.add_subplot(gs[0, 1])
+        plot_f_path(ax, f, Y)
+        if PLOT_BOTH:
+            ax = fig.add_subplot(gs[0, 2:])
+        else:
+            ax = fig.add_subplot(gs[1, :])
         if nnd_f:
-            nnd_f = np.array(nnd_f)
-            ax.errorbar(iterations, nnd_f[:, 0], nnd_f[:, 1], label="$\\overline{f_{min}(x)}$")
-            ax.plot(iterations, [SIGMA_A*anneal(i) for i in iterations], label="$\\sigma_A$")
-            ax.hlines(1/K, 0, iterations[-1], linestyles="dashed", label=f"$1/K = 1/{K}$")
+            plot_f_wstats(ax, nnd_f)
+
+    if PLOT_G:
+        G_ROW = 1 if PLOT_F else 0
+        if PLOT_3D:
+            ax = fig.add_subplot(gs[G_ROW, 0], projection="3d")
+            plot_g_3d(ax, g)
+        ax = fig.add_subplot(gs[G_ROW, 1])
+        plot_g_path(ax, g)
+        if PLOT_BOTH:
+            ax = fig.add_subplot(gs[G_ROW, 2:])
+        else:
+            ax = fig.add_subplot(gs[G_ROW+1, :])
         if nnd_g:
-            nnd_g = np.array(nnd_g)
-            ax.errorbar(iterations, nnd_g[:, 0], nnd_g[:, 1], label="$\\overline{g_{min}(y)}$")
-            ax.plot(iterations, [SIGMA_B*anneal(i) for i in iterations], label="$\\sigma_B$")
-            ax.hlines(1/K**2, 0, iterations[-1], linestyles="dashed", label=f"$1/K^2 = 1/{K**2}$")
-        ax.legend()
-    index += 1
+            plot_g_wstats(ax, nnd_g)
+
     plt.show()
+
+def plot_f_3d(ax, f):
+    ax.set_xlabel("$x$")
+    ax.set_ylabel("$y_0$")
+    ax.set_zlabel("$y_1$")
+    for x, fx in enumerate(f):
+        ax.plot([x, x], [0, fx[0]*(K-1)], [0, fx[1]*(K-1)], "b-")
+
+def plot_f_path(ax, f, Y):
+    ax.set_aspect("equal")
+    ax.set_xlabel("$y_0$")
+    ax.set_ylabel("$y_1$")
+    ax.grid()
+    path = Y[np.argsort(best_match_f(f, Y))]
+    path = path[:,0], path[:,1]
+    ax.plot(*path, ".r-")
+    ax.scatter(f[:,0]*(K-1), f[:,1]*(K-1), c="b", marker="o")
+
+def plot_f_wstats(ax, wstats):
+    iterations = np.arange(len(wstats))
+    wstats = np.array(wstats)
+    ax.grid()
+    ax.plot(iterations, wstats[:, 0], label="Median inter-node distance")
+    ax.plot(iterations, wstats[:, 1], label="Min inter-node distance")
+    ax.plot(iterations, wstats[:, 2], label="Max inter-node distance")
+    ax.plot(iterations, [SIGMA_A*anneal(i) for i in iterations], label="$\\sigma_A$")
+    ax.hlines(1/K, 0, iterations[-1], linestyles="dashed", label=f"$1/K = 1/{K}$")
+    ax.legend()
+
+def plot_g_3d(ax, g):
+    ax.set_xlabel("$y_0$")
+    ax.set_ylabel("$y_1$")
+    ax.set_zlabel("$x$")
+    X_, Y_ = np.arange(K), np.arange(K)
+    X_, Y_ = np.meshgrid(X_, Y_)
+    ax.plot_surface(X_, Y_, g, cmap=cm.coolwarm)
+
+def plot_g_path(ax, g):
+    X = np.arange(K**2)
+    ax.set_xlabel("$y_0$")
+    ax.set_ylabel("$y_1$")
+    ax.grid()
+    path = best_match_g(g, X)
+    path = path[:,1], path[:,0]
+    ax.plot(*path, ".g-")
+
+def plot_g_wstats(ax, wstats):
+    iterations = np.arange(len(wstats))
+    wstats = np.array(wstats)
+    ax.grid()
+    ax.plot(iterations, wstats[:, 0], label="Median inter-node distance")
+    ax.plot(iterations, wstats[:, 1], label="Min inter-node distance")
+    ax.plot(iterations, wstats[:, 2], label="Max inter-node distance")
+    ax.plot(iterations, [SIGMA_B*anneal(i) for i in iterations], label="$\\sigma_B$")
+    ax.hlines(1/K**2, 0, iterations[-1], linestyles="dashed", label=f"$1/K^2 = 1/{K**2}$")
+    ax.legend()
 
 
 if __name__ == "__main__":
